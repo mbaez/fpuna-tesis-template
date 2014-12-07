5506d0b4e694480c4d894a5436f7a811e70b022b
Maximiliano Báez González <mxbg.py@gmail.com>
2014-12-07 12:44:10 -0300

    Se actualiza el script de generación de la documentación

--- generate.sh
+++ generate.sh
@@ -18,8 +18,12 @@ clean(){
     find . -type f -name "*.fdb_latexmk" -exec rm -f {} \;
     find . -type f -name "*.orig" -exec rm -f {} \;
     find . -type f -name "*.loa" -exec rm -f {} \;
-    #~ rm *.log *.bbl *.blg *.bcf *.run.xml *.lof *.lot *.toc e *.fdb_latexmk *.orig *.loa
+    find . -type f -name "*.snm" -exec rm -f {} \;
+    find . -type f -name "*.nav" -exec rm -f {} \;
+    find . -type f -name "*.out" -exec rm -f {} \;
 }
+
+
 #~ Funcion que costruye
 build(){
     pdflatex $NAME
@@ -29,9 +33,47 @@ build(){
     evince $NAME.pdf&
 }
 
+#~ Funcion que costruye el ppt
+build_ppt(){
+    pdflatex $NAME
+    evince $NAME.pdf&
+}
 
 if [ -n "$1" ] && [ "$1" = "clean" ]
 then
+    clean
+
+elif [ -n "$1" ] && [ "$1" = "paper" ]
+then
+    cd ieee-paper
+    NAME=paper-i3e
+    echo $NAME
+    clean
+    build
+    clean
+
+elif [ -n "$1" ] && [ "$1" = "ppt" ]
+then
+    cd presentacion
+    NAME=presentacion
+    echo $NAME
+    clean
+    build_ppt
+    clean
+
+elif [ -n "$1" ] && [ "$1" = "all" ]
+then
+    cd book
+    clean
+    build
+    cd ../ieee-paper
+    NAME=paper-i3e
+    echo $NAME
+    build
+    cd ../presentacion
+    NAME=presentacion
+    echo $NAME
+    build_ppt
     clean
 elif [ -n "$1" ]
 then
@@ -41,6 +83,7 @@ then
     build
     clean
 else
+    cd book
     clean
     build
     clean
